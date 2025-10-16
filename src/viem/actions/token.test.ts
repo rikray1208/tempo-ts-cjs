b04eb8278b2b88636311b1821c2238024af89f84
jxom <7336481+jxom@users.noreply.github.com>
2025-10-17 08:03:32 +1100

    feat: rename transferPolicy

--- src/viem/actions/token.test.ts
+++ src/viem/actions/token.test.ts
@@ -317,7 +317,7 @@ describe('getMetadata', () => {
         "supplyCap": 115792089237316195423570985008687907853269984665640564039457584007913129639935n,
         "symbol": "TestUSD",
         "totalSupply": 340282366920938463647842048168863727605n,
-        "transferPolicy": "always-allow",
+        "transferPolicyId": 1n,
       }
     `)
   })
@@ -342,7 +342,7 @@ describe('getMetadata', () => {
         "supplyCap": 115792089237316195423570985008687907853269984665640564039457584007913129639935n,
         "symbol": "TUSD",
         "totalSupply": 0n,
-        "transferPolicy": "always-allow",
+        "transferPolicyId": 1n,
       }
     `)
   })
@@ -367,7 +367,7 @@ describe('getMetadata', () => {
         "supplyCap": 115792089237316195423570985008687907853269984665640564039457584007913129639935n,
         "symbol": "TUSD",
         "totalSupply": 0n,
-        "transferPolicy": "always-allow",
+        "transferPolicyId": 1n,
       }
     `)
   })
